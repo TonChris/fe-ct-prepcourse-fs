37a5f8a86c63a4ab70f3e3a6ac9ab235b6e59aa7
Josè Guevara <jmguevara1812@gmail.com>
2023-05-12 13:32:42 -0400

    Vamos

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -14,7 +14,7 @@ const nuevoNumero = 1985;
 const nuevoBoolean = true;
 
 // Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - null === 3;
+const nuevaResta = 10 - 7 === 3;
 
 // Resuelve el siguiente problema matemático.
 const nuevaMultiplicacion = 10 * null === 40;
